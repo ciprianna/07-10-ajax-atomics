87f0c8865d3c55231f15adbd2a49b69e60126085
Ciprianna <ciprianna17@gmail.com>
2015-07-12 17:42:45 -0500

    Exercises 1 and 2 are done.

--- 04/public/global.js
+++ 04/public/global.js
@@ -2,17 +2,19 @@ var myLink = document.getElementById("clicker");
 
 var handleTheClick = function(event){
   var thisLink = this;
-  
+
   var req = new XMLHttpRequest();
 
-  req.open("get", "/hey_there");
+  var user_typed = document.getElementById('word').value;
 
-  req.addEventListener("load", function(){    
+  req.open("get", user_typed);
+
+  req.addEventListener("load", function(){
     thisLink.innerText = this.response;
   });
 
   req.send();
-  
+
   // Prevent the link from refreshing the DOM.
   event.preventDefault();
 }
