1283b0015120c8c0d6ba0cfe4cbce926760fa6fa
Ciprianna <ciprianna17@gmail.com>
2015-07-12 16:45:04 -0500

    Atomic 01 complete.

--- 01/public/global.js
+++ 01/public/global.js
@@ -1,14 +1,18 @@
 var req = new XMLHttpRequest();
 
 // For 'Exercise 2', you will modify this line:
-req.open("get", "/info1");
+req.open("get", "/info2");
 
 req.addEventListener("loadstart", function(){
   console.log("loadstart");
 });
 
 req.addEventListener("load", function(){
-  // Your code for Exercise 1 goes here.
+  if (this.response === "yes") {
+    alert("Yay!")
+  } else {
+    alert("Aw, shucks!")
+  }
 });
 
 req.send();
